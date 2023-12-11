c926aca195405dcfcc23fc428aac52341f932234
Alex Agosto Figueroa <alex.agosto@upr.edu>
2023-12-11 14:55:28 -0400

    Delete ProjectFolder directory

--- ProjectFolder/config/connectHeroku.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(
-    database="d4b4cfrn0mrp8p",
-    user="mzytobqvjwsson",
-    password="b2a8350a67754fe49c4c3ab554e3b13a5d258d587826c7ecab8a4169c064699c",
-    host="ec2-34-202-53-101.compute-1.amazonaws.com",
-    port="5432"
-)
